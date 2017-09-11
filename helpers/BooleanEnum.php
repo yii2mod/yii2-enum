3f93e105f35530d267f0968968b6fd7029b2bd56
igor-chepurnoi <chepurnoi.igor@gmail.com>
2017-09-11 17:03:06 +0300

    added phpunit to require-dev section in the composer.json, update travis.yml

--- helpers/BooleanEnum.php
+++ helpers/BooleanEnum.php
@@ -12,6 +12,9 @@ class BooleanEnum extends BaseEnum
     const YES = 1;
     const NO = 0;
 
+    /**
+     * @var array
+     */
     public static $list = [
         self::YES => 'Yes',
         self::NO => 'No',
